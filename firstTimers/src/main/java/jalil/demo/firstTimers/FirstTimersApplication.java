23dbf608544ffe44744e1168c27e1148c345bfb3
jarjanazy <celil.cercenazi@gmail.com>
2021-02-13 14:03:46 +0300

    init the repo with an empty spring boot project

---
+++ firstTimers/src/main/java/jalil/demo/firstTimers/FirstTimersApplication.java
@@ -0,0 +1,13 @@
+package jalil.demo.firstTimers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstTimersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstTimersApplication.class, args);
+	}
+
+}
---
+++ firstTimers/src/test/java/jalil/demo/firstTimers/FirstTimersApplicationTests.java
@@ -0,0 +1,13 @@
+package jalil.demo.firstTimers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstTimersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
